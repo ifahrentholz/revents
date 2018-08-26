8a3b721103b7010734d3d47a2dfe801a8c2963d8
ifahrentholz <ingof.work@gmail.com>
2018-08-26 21:43:56 +0200

    Use redux-form implicit initialvalues handling and submit handling

--- src/app/features/event/EventForm/EventForm.jsx
+++ src/app/features/event/EventForm/EventForm.jsx
@@ -11,19 +11,15 @@ import SelectInput from '../../../common/form/SelectInput';
 const mapStateToProps = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
 
-  let event = {
-    title: '',
-    date: '',
-    city: '',
-    venue: '',
-    hostedBy: ''
-  };
+  let event = {};
 
   if (eventId && state.events.length > 0) {
     event = state.events.filter(event => event.id === eventId)[0];
   }
 
-  return { event };
+  return {
+    initialValues: event
+  };
 };
 
 const mapDispatchToProps = {
@@ -42,18 +38,18 @@ const category = [
 
 class EventForm extends Component {
   /**
-   * @param {Event} evt
+   * @param {Object} values
    */
-  onFormSubmit = evt => {
-    evt.preventDefault();
-    if (this.state.event.id) {
-      this.props.updateEvent(this.state.event);
+  onFormSubmit = values => {
+    if (this.props.initialValues.id) {
+      this.props.updateEvent(values);
       this.props.history.goBack();
     } else {
       const newEvent = {
-        ...this.state.event,
+        ...values,
         id: cuid(),
-        hostPhotoURL: '/assets/user.png'
+        hostPhotoURL: '/assets/user.png',
+        hostedBy: 'Bob'
       };
       this.props.createEvent(newEvent);
       this.props.history.push('/events');
@@ -66,7 +62,7 @@ class EventForm extends Component {
         <Grid.Column width={10}>
           <Segment>
             <Header sub color="teal" content="Event Details" />
-            <Form onSubmit={this.onFormSubmit}>
+            <Form onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
               <Field
                 name="title"
                 type="text"
@@ -108,4 +104,4 @@ class EventForm extends Component {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(reduxForm({ form: 'eventForm' })(EventForm));
+)(reduxForm({ form: 'eventForm', enableReinitialize: true })(EventForm));
